80fd76a4d7147a8bc2ec5fead3014a8f260ed3f0
MrMalekfar <134326544+MrMalekfar@users.noreply.github.com>
2023-12-12 03:53:57 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var cfIPv4 = []
 var cfIPv4ToScan = []
-const noOfEachRange24 = 90
+const noOfEachRange24 = 150
 const client = new XMLHttpRequest();
 client.open('GET', 'https://raw.githubusercontent.com/MrMalekfar/cf-ip-scanner/main/ipv4.txt');
 client.onreadystatechange = function() {
